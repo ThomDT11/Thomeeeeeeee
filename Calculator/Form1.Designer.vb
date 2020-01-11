31cdbf3ac7ed2e5a6f83eb8e8c8818c6a4ec7c1b
Your Name <you@example.com>
2020-01-11 14:18:20 +0800

    Third Commit w/ Design

--- Calculator/Form1.Designer.vb
+++ Calculator/Form1.Designer.vb
@@ -47,123 +47,135 @@ Partial Class Form1
         '
         'Button1
         '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button1.Location = New System.Drawing.Point(21, 289)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(82, 71)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.UseVisualStyleBackColor = False
         '
         'Button2
         '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button2.Location = New System.Drawing.Point(122, 289)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(82, 71)
         Me.Button2.TabIndex = 1
         Me.Button2.Text = "2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.UseVisualStyleBackColor = False
         '
         'Button3
         '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button3.Location = New System.Drawing.Point(223, 289)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(82, 71)
         Me.Button3.TabIndex = 3
         Me.Button3.Text = "3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.UseVisualStyleBackColor = False
         '
         'Button4
         '
+        Me.Button4.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button4.Location = New System.Drawing.Point(21, 389)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(82, 71)
         Me.Button4.TabIndex = 2
         Me.Button4.Text = "4"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.Button4.UseVisualStyleBackColor = False
         '
         'Button5
         '
+        Me.Button5.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button5.Location = New System.Drawing.Point(122, 389)
         Me.Button5.Name = "Button5"
         Me.Button5.Size = New System.Drawing.Size(82, 71)
         Me.Button5.TabIndex = 7
         Me.Button5.Text = "5"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.Button5.UseVisualStyleBackColor = False
         '
         'Button6
         '
+        Me.Button6.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button6.Location = New System.Drawing.Point(223, 389)
         Me.Button6.Name = "Button6"
         Me.Button6.Size = New System.Drawing.Size(82, 71)
         Me.Button6.TabIndex = 6
         Me.Button6.Text = "6"
-        Me.Button6.UseVisualStyleBackColor = True
+        Me.Button6.UseVisualStyleBackColor = False
         '
         'Button7
         '
+        Me.Button7.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button7.Location = New System.Drawing.Point(21, 483)
         Me.Button7.Name = "Button7"
         Me.Button7.Size = New System.Drawing.Size(82, 71)
         Me.Button7.TabIndex = 5
         Me.Button7.Text = "7"
-        Me.Button7.UseVisualStyleBackColor = True
+        Me.Button7.UseVisualStyleBackColor = False
         '
         'Button8
         '
+        Me.Button8.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button8.Location = New System.Drawing.Point(122, 483)
         Me.Button8.Name = "Button8"
         Me.Button8.Size = New System.Drawing.Size(82, 71)
         Me.Button8.TabIndex = 4
         Me.Button8.Text = "8"
-        Me.Button8.UseVisualStyleBackColor = True
+        Me.Button8.UseVisualStyleBackColor = False
         '
         'Button9
         '
+        Me.Button9.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button9.Location = New System.Drawing.Point(223, 483)
         Me.Button9.Name = "Button9"
         Me.Button9.Size = New System.Drawing.Size(82, 71)
         Me.Button9.TabIndex = 11
         Me.Button9.Text = "9"
-        Me.Button9.UseVisualStyleBackColor = True
+        Me.Button9.UseVisualStyleBackColor = False
         '
         'Button10
         '
+        Me.Button10.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.Button10.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button10.Location = New System.Drawing.Point(122, 577)
         Me.Button10.Name = "Button10"
         Me.Button10.Size = New System.Drawing.Size(82, 71)
         Me.Button10.TabIndex = 10
         Me.Button10.Text = "0"
-        Me.Button10.UseVisualStyleBackColor = True
+        Me.Button10.UseVisualStyleBackColor = False
         '
         'Button11
         '
+        Me.Button11.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
         Me.Button11.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button11.Location = New System.Drawing.Point(122, 200)
         Me.Button11.Name = "Button11"
         Me.Button11.Size = New System.Drawing.Size(82, 71)
         Me.Button11.TabIndex = 9
         Me.Button11.Text = "CE"
-        Me.Button11.UseVisualStyleBackColor = True
+        Me.Button11.UseVisualStyleBackColor = False
         '
         'Button12
         '
+        Me.Button12.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
         Me.Button12.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button12.Location = New System.Drawing.Point(223, 200)
         Me.Button12.Name = "Button12"
         Me.Button12.Size = New System.Drawing.Size(82, 71)
         Me.Button12.TabIndex = 8
         Me.Button12.Text = "C"
-        Me.Button12.UseVisualStyleBackColor = True
+        Me.Button12.UseVisualStyleBackColor = False
         '
         'TextBox1
         '
@@ -178,88 +190,97 @@ Partial Class Form1
         '
         'Button13
         '
+        Me.Button13.BackColor = System.Drawing.SystemColors.GradientActiveCaption
         Me.Button13.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button13.Location = New System.Drawing.Point(323, 577)
         Me.Button13.Name = "Button13"
         Me.Button13.Size = New System.Drawing.Size(82, 71)
         Me.Button13.TabIndex = 16
         Me.Button13.Text = "+"
-        Me.Button13.UseVisualStyleBackColor = True
+        Me.Button13.UseVisualStyleBackColor = False
         '
         'Button14
         '
+        Me.Button14.BackColor = System.Drawing.SystemColors.GradientActiveCaption
         Me.Button14.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button14.Location = New System.Drawing.Point(323, 483)
         Me.Button14.Name = "Button14"
         Me.Button14.Size = New System.Drawing.Size(82, 71)
         Me.Button14.TabIndex = 15
         Me.Button14.Text = "-"
-        Me.Button14.UseVisualStyleBackColor = True
+        Me.Button14.UseVisualStyleBackColor = False
         '
         'Button15
         '
+        Me.Button15.BackColor = System.Drawing.SystemColors.GradientActiveCaption
         Me.Button15.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button15.Location = New System.Drawing.Point(323, 289)
         Me.Button15.Name = "Button15"
         Me.Button15.Size = New System.Drawing.Size(82, 71)
         Me.Button15.TabIndex = 14
         Me.Button15.Text = "/"
-        Me.Button15.UseVisualStyleBackColor = True
+        Me.Button15.UseVisualStyleBackColor = False
         '
         'Button16
         '
+        Me.Button16.BackColor = System.Drawing.SystemColors.GradientActiveCaption
         Me.Button16.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
-        Me.Button16.Location = New System.Drawing.Point(323, 389)
+        Me.Button16.Location = New System.Drawing.Point(334, 389)
         Me.Button16.Name = "Button16"
         Me.Button16.Size = New System.Drawing.Size(82, 71)
         Me.Button16.TabIndex = 13
         Me.Button16.Text = "X"
-        Me.Button16.UseVisualStyleBackColor = True
+        Me.Button16.UseVisualStyleBackColor = False
         '
         'Button17
         '
+        Me.Button17.BackColor = System.Drawing.SystemColors.GrayText
         Me.Button17.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button17.Location = New System.Drawing.Point(223, 577)
         Me.Button17.Name = "Button17"
         Me.Button17.Size = New System.Drawing.Size(82, 71)
         Me.Button17.TabIndex = 20
         Me.Button17.Text = "="
-        Me.Button17.UseVisualStyleBackColor = True
+        Me.Button17.UseVisualStyleBackColor = False
         '
         'Button18
         '
+        Me.Button18.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.Button18.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button18.Location = New System.Drawing.Point(21, 200)
         Me.Button18.Name = "Button18"
         Me.Button18.Size = New System.Drawing.Size(82, 71)
         Me.Button18.TabIndex = 19
         Me.Button18.Text = "%"
-        Me.Button18.UseVisualStyleBackColor = True
+        Me.Button18.UseVisualStyleBackColor = False
         '
         'Button19
         '
-        Me.Button19.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.Button19.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button19.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
         Me.Button19.Location = New System.Drawing.Point(323, 200)
         Me.Button19.Name = "Button19"
         Me.Button19.Size = New System.Drawing.Size(82, 71)
         Me.Button19.TabIndex = 18
         Me.Button19.Text = "Del"
-        Me.Button19.UseVisualStyleBackColor = True
+        Me.Button19.UseVisualStyleBackColor = False
         '
         'Button20
         '
+        Me.Button20.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.Button20.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.Button20.Location = New System.Drawing.Point(21, 577)
         Me.Button20.Name = "Button20"
         Me.Button20.Size = New System.Drawing.Size(82, 71)
         Me.Button20.TabIndex = 17
         Me.Button20.Text = "."
-        Me.Button20.UseVisualStyleBackColor = True
+        Me.Button20.UseVisualStyleBackColor = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
         Me.ClientSize = New System.Drawing.Size(632, 700)
         Me.Controls.Add(Me.Button12)
         Me.Controls.Add(Me.Button17)
